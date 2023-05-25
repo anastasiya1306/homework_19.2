88fed4f04303f3fcdb44459c7e1f476099ad2aa1
Anastasiya Burykh <burykh_2000@mail.ru>
2023-05-25 22:33:22 +0300

    hw_20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import get_home, get_contacts
+from catalog.views import get_home, get_contacts, product
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', get_home),
-    path('contacts/', get_contacts),
+    path('', get_home, name='home'),
+    path('contacts/', get_contacts, name='contacts'),
+    path('product_card/<int:pk>/', product, name='product_item')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
+
 def get_home(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Product.objects.all()
+    }
+    return render(request, 'catalog/home.html', context)
 
 def get_contacts(request):
     return render(request, 'catalog/contacts.html')
+
+def product(request, pk):
+    product_item = Product.objects.get(pk=pk)
+    context = {
+        'object': product_item,
+        'title': product_item,
+    }
+    return render(request, 'catalog/product_card.html', context)
