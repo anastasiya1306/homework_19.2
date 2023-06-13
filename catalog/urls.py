fea7cab40025c1d5481d5e2759d967dc5d8e21df
Anastasiya Burykh <burykh_2000@mail.ru>
2023-06-13 20:48:31 +0300

    hw_21.1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import get_home, get_contacts, product
+from catalog.views import get_contacts, ProductListView, ProductDetailView, BlogListView, BlogDetailView, \
+    BlogCreateView, BlogUpdateView, BlogDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', get_home, name='home'),
+    path('', ProductListView.as_view(), name='home'),
     path('contacts/', get_contacts, name='contacts'),
-    path('product_card/<int:pk>/', product, name='product_item')
+    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_item'),
+    path('blogs/', BlogListView.as_view(), name='blog_list'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_item'),
+    path('blogs/create/', BlogCreateView.as_view(), name='blog_create'),
+    path('blogs/update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),
+    path('blogs/delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
 ]
