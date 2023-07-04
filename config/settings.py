2289f78d7939d1a1652eec960f7d5f78f430e0ad
Anastasiya Burykh <burykh_2000@mail.ru>
2023-07-04 19:59:40 +0300

    hw_22.2

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-o!@c@x@8fu$7j%4xl*i)@^3tv7&vi)9^mko**l-h3i7*tuw&@%
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -83,7 +80,6 @@ DATABASES = {
         'PASSWORD': 'simba2106',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -103,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -114,7 +109,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -132,5 +126,19 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'users.User'
+#настройка хост-сервера электронной почты в для письма с подтверждением
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465   #порт SMTP для yandex
+EMAIL_HOST_USER = 'anastasiyab2010@yandex.ru'
+EMAIL_HOST_PASSWORD = 'pzzuhavilkmpwnct'    #полученный пароль для приложения
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True  #использовать защищенное соединение
 
+EMAIL_SERVER = EMAIL_HOST_USER    #email сервера
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER    #с какого адреса будут отправляться письма
+EMAIL_ADMIN = EMAIL_HOST_USER    #email, куда будут все данные приходить с сайта
+
+AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
